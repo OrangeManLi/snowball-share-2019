d02adf172ebe524da3cf05501192b0167b8143af
OrangeManLi <OrangeManLi@163.com>
2019-10-16 07:27:38 +0000

    Deploying to gh-pages from master 5b684a9b9c692955efa1e55ca5d0c58954e64af2

---
+++ precache-manifest.4ccb58acd4dd8c3a3b765c443b2a70b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e40d30824e7e8cf9c7acf594e72b7d",
+    "url": "/snowball-share-2019/index.html"
+  },
+  {
+    "revision": "39e9f2918018011ec629",
+    "url": "/snowball-share-2019/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7d1fd9398dfe6b3e72b3",
+    "url": "/snowball-share-2019/static/js/2.07262e6e.chunk.js"
+  },
+  {
+    "revision": "39e9f2918018011ec629",
+    "url": "/snowball-share-2019/static/js/main.8144af58.chunk.js"
+  },
+  {
+    "revision": "74c4f44c9538566a64b3",
+    "url": "/snowball-share-2019/static/js/runtime-main.6027a4cd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/snowball-share-2019/static/media/logo.25bf045c.svg"
+  }
+]);
